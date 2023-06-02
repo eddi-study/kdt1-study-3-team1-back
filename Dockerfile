95f8f8d8a84dcac4710a49b8369f2f0099a0538a
Hakgyu Kim <zage4448@gmail.com>
2023-06-02 15:30:18 +0900

    del: [김학규] 배포용 코드 삭제

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:17-slim
-
-ARG JAR_FILE=/build/libs/*.jar
-COPY ${JAR_FILE} app.jar
-
-ADD https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh /
-RUN chmod +x /wait-for-it.sh
--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,8 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080",
-                                "http://localhost:8080",
-                                "http://3.37.125.157")
+                                "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
