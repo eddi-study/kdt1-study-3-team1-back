2aeb99c19ac456617ca2a97f5839ab26cb4561fb
Hakgyu Kim <zage4448@gmail.com>
2023-06-02 11:51:06 +0900

    fix: [김학규] corsconfig fix

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")
-                .allowedOrigins("http://localhost:8080")
+//                .allowedOrigins("http://127.0.0.1:8080")
+//                .allowedOrigins("http://localhost:8080")
                 .allowedOrigins("http://3.37.125.157")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
